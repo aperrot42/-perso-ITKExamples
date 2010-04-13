69cf40fadf38ae6fdf00d1f556ca192f74d89920
Antonin Perrot-Audet <antonin07130@gmail.com>
2010-04-13 17:30:54 -0400

    Add Thresholding sources and Cmakelist.txt

---
+++ Thresholding/Thresholding.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : ThresholdingITKex.cpp
+// Author      : Antonin
+// Version     : 0.1
+//============================================================================
+
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkImageRegionConstIterator.h"
+#include "itkImageRegionIterator.h"
+
+#include <iostream>
+
+int main()
+{}
+
